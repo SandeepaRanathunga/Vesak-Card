c95a551cb40ad7fa02855b47a1eb0b2358f812ad
Sandeepa Ranathunga <sandeeparanatungavcg@gmail.com>
2021-05-27 02:47:57 +0530

    all the linking part done.card has to be made

--- js/index.js
+++ js/index.js
@@ -3,5 +3,19 @@ document.getElementById('form').addEventListener('submit', (e) => {
   document.querySelector('.form_container').style.display = 'none';
   const sender = document.getElementById('sender').value;
   const receiver = document.getElementById('receiver').value;
+  const view_link = `http://127.0.0.1:5500/view_card.html?sender=${sender}&receiver=${receiver}`;
+  document.getElementById('link').value = view_link;
+  document.getElementById('show_card').href = view_link;
+  document.getElementById(
+    'whatsapp'
+  ).href = `whatsapp://send?text=${view_link}`;
   document.querySelector('.action_container').style.display = 'flex';
+});
+
+document.getElementById('link_copy').addEventListener('click', () => {
+  const link = document.getElementById('link');
+  link.select();
+  link.setSelectionRange(0, 99999);
+  document.execCommand('copy');
+  alert(link.value);
 });
